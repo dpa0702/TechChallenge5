a9db14dd2b7655e616f9a15fe924aa029cddf675
demetrios arghirachis <dpa0702@gmail.com>
2024-06-18 12:15:11 -0300

    Adicionando testes....

--- GestaoInvestimentosTests/Entities/TransacaoTest.cs
+++ GestaoInvestimentosTests/Entities/TransacaoTest.cs
@@ -1,7 +1,12 @@
 ﻿using Bogus;
+using GestaoInvestimentosCore.DTO.Transacao;
+using GestaoInvestimentosCore.Entities;
+using GestaoInvestimentosCore.Enums;
+using GestaoInvestimentosTests.Entities.Mock;
 
 namespace GestaoInvestimentosTests.Entities
 {
+    [TestClass]
     public class TransacaoTest
     {
         private readonly Faker _faker;
@@ -10,5 +15,185 @@ namespace GestaoInvestimentosTests.Entities
         {
             _faker = new Faker();
         }
+
+        #region Method Insert Tests
+        [TestMethod]
+        public void ValidateInserTransacao_ShouldReturnSuccessAtivoId()
+        {
+            // Arrange
+            var TransacaoDtoMock = new MockTransacaoDTO();
+            TransacaoDtoMock.AtivoId = 1;
+
+            // Act
+            var TransacaoDto = new CreateTransacaoDTO
+            {
+                AtivoId = TransacaoDtoMock.AtivoId,
+                PortfolioId = TransacaoDtoMock.PortfolioId,
+                TipoTransacao = TransacaoDtoMock.TipoTransacao,
+                Preco = TransacaoDtoMock.Preco,
+                Quantidade = TransacaoDtoMock.Quantidade,
+                DataTransacao = TransacaoDtoMock.DataTransacao,
+            };
+
+            // Assert
+            Assert.AreEqual(TransacaoDtoMock.DataTransacao, TransacaoDto.DataTransacao);
+        }
+
+        [TestMethod]
+        public void ValidateInserTransacao_ShouldReturnSuccessPortfolioId()
+        {
+            // Arrange
+            var TransacaoDtoMock = new MockTransacaoDTO();
+            TransacaoDtoMock.PortfolioId = 1;
+
+            // Act
+            var TransacaoDto = new CreateTransacaoDTO
+            {
+                AtivoId = TransacaoDtoMock.AtivoId,
+                PortfolioId = TransacaoDtoMock.PortfolioId,
+                TipoTransacao = TransacaoDtoMock.TipoTransacao,
+                Preco = TransacaoDtoMock.Preco,
+                Quantidade = TransacaoDtoMock.Quantidade,
+                DataTransacao = TransacaoDtoMock.DataTransacao,
+            };
+
+            // Assert
+            Assert.AreEqual(TransacaoDtoMock.DataTransacao, TransacaoDto.DataTransacao);
+        }
+
+        [TestMethod]
+        public void ValidateInserTransacao_ShouldReturnSuccessTipoTransacaoVenda()
+        {
+            // Arrange
+            var TransacaoDtoMock = new MockTransacaoDTO();
+            TransacaoDtoMock.TipoTransacao = TipoTransacao.Venda;
+
+            // Act
+            var TransacaoDto = new CreateTransacaoDTO
+            {
+                AtivoId = TransacaoDtoMock.AtivoId,
+                PortfolioId = TransacaoDtoMock.PortfolioId,
+                TipoTransacao = TransacaoDtoMock.TipoTransacao,
+                Preco = TransacaoDtoMock.Preco,
+                Quantidade = TransacaoDtoMock.Quantidade,
+                DataTransacao = TransacaoDtoMock.DataTransacao,
+            };
+
+            // Assert
+            Assert.AreEqual(TransacaoDtoMock.DataTransacao, TransacaoDto.DataTransacao);
+        }
+
+        [TestMethod]
+        public void ValidateInserTransacao_ShouldReturnSuccessTipoTransacaoCompra()
+        {
+            // Arrange
+            var TransacaoDtoMock = new MockTransacaoDTO();
+            TransacaoDtoMock.TipoTransacao = TipoTransacao.Compra;
+
+            // Act
+            var TransacaoDto = new CreateTransacaoDTO
+            {
+                AtivoId = TransacaoDtoMock.AtivoId,
+                PortfolioId = TransacaoDtoMock.PortfolioId,
+                TipoTransacao = TransacaoDtoMock.TipoTransacao,
+                Preco = TransacaoDtoMock.Preco,
+                Quantidade = TransacaoDtoMock.Quantidade,
+                DataTransacao = TransacaoDtoMock.DataTransacao,
+            };
+
+            // Assert
+            Assert.AreEqual(TransacaoDtoMock.DataTransacao, TransacaoDto.DataTransacao);
+        }
+
+        [TestMethod]
+        public void ValidateInserTransacao_ShouldReturnSuccessPreco()
+        {
+            // Arrange
+            var TransacaoDtoMock = new MockTransacaoDTO();
+            TransacaoDtoMock.Preco = 1000;
+
+            // Act
+            var TransacaoDto = new CreateTransacaoDTO
+            {
+                AtivoId = TransacaoDtoMock.AtivoId,
+                PortfolioId = TransacaoDtoMock.PortfolioId,
+                TipoTransacao = TransacaoDtoMock.TipoTransacao,
+                Preco = TransacaoDtoMock.Preco,
+                Quantidade = TransacaoDtoMock.Quantidade,
+                DataTransacao = TransacaoDtoMock.DataTransacao,
+            };
+
+            // Assert
+            Assert.AreEqual(TransacaoDtoMock.DataTransacao, TransacaoDto.DataTransacao);
+        }
+
+        [TestMethod]
+        public void ValidateInserTransacao_ShouldReturnSuccessQuantidade()
+        {
+            // Arrange
+            var TransacaoDtoMock = new MockTransacaoDTO();
+            TransacaoDtoMock.Quantidade = 100;
+
+            // Act
+            var TransacaoDto = new CreateTransacaoDTO
+            {
+                AtivoId = TransacaoDtoMock.AtivoId,
+                PortfolioId = TransacaoDtoMock.PortfolioId,
+                TipoTransacao = TransacaoDtoMock.TipoTransacao,
+                Preco = TransacaoDtoMock.Preco,
+                Quantidade = TransacaoDtoMock.Quantidade,
+                DataTransacao = TransacaoDtoMock.DataTransacao,
+            };
+
+            // Assert
+            Assert.AreEqual(TransacaoDtoMock.DataTransacao, TransacaoDto.DataTransacao);
+        }
+
+        [TestMethod]
+        public void ValidateInserTransacao_ShouldReturnSuccessDataTransacao()
+        {
+            // Arrange
+            var TransacaoDtoMock = new MockTransacaoDTO();
+            TransacaoDtoMock.DataTransacao = DateTime.Now;
+
+            // Act
+            var TransacaoDto = new CreateTransacaoDTO
+            {
+                AtivoId = TransacaoDtoMock.AtivoId,
+                PortfolioId = TransacaoDtoMock.PortfolioId,
+                TipoTransacao = TransacaoDtoMock.TipoTransacao,
+                Preco = TransacaoDtoMock.Preco,
+                Quantidade = TransacaoDtoMock.Quantidade,
+                DataTransacao = TransacaoDtoMock.DataTransacao,
+            };
+
+            // Assert
+            Assert.AreEqual(TransacaoDtoMock.DataTransacao, TransacaoDto.DataTransacao);
+        }
+
+        [TestMethod]
+        public void ValidateInserTransacao_ShouldReturnSuccess()
+        {
+            // Arrange
+            var TransacaoDtoMock = new MockTransacaoDTO();
+
+            // Act
+            var TransacaoDto = new CreateTransacaoDTO { AtivoId = TransacaoDtoMock.AtivoId,
+                PortfolioId = TransacaoDtoMock.PortfolioId,
+                TipoTransacao = TransacaoDtoMock.TipoTransacao,
+                Preco = TransacaoDtoMock.Preco,
+                Quantidade = TransacaoDtoMock.Quantidade,
+                DataTransacao = TransacaoDtoMock.DataTransacao,
+                };
+
+            // Assert
+            Assert.AreEqual(TransacaoDtoMock.AtivoId, TransacaoDto.AtivoId);
+            Assert.AreEqual(TransacaoDtoMock.PortfolioId, TransacaoDto.PortfolioId);
+            Assert.AreEqual(TransacaoDtoMock.TipoTransacao, TransacaoDto.TipoTransacao);
+            Assert.AreEqual(TransacaoDtoMock.Preco, TransacaoDto.Preco);
+            Assert.AreEqual(TransacaoDtoMock.Quantidade, TransacaoDto.Quantidade);
+            Assert.AreEqual(TransacaoDtoMock.DataTransacao, TransacaoDto.DataTransacao);
+        }
+        #endregion
     }
 }
--- GestaoInvestimentosTests/Services/AtivoServiceTest.cs
+++ GestaoInvestimentosTests/Services/AtivoServiceTest.cs
@@ -5,6 +5,10 @@ using Moq;
 using GestaoInvestimentosCore.Interfaces.Repository;
 using GestaoInvestimentosCore.Services;
 using GestaoInvestimentosCore.Entities;
+using GestaoInvestimentosCore.DTO.Ativo;
+using GestaoInvestimentosTests.Entities.Mock;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Runtime.ConstrainedExecution;
 
 namespace GestaoInvestimentosTests.Services
 {
@@ -53,6 +57,78 @@ namespace GestaoInvestimentosTests.Services
             {
                 Assert.AreEqual("Erro na camada de serviço ao consultar Ativo por id. Mensagem de Erro: Repository exception", ex.Message);
             }
+        }
+
+        [TestMethod]
+        public void AtivoListByFilter_ShouldReturnSuccess()
+        {
+            // Arrange
+
+            var autoFixture = new Fixture().Customize(new AutoMoqCustomization());
+            int idFilter = It.IsAny<int>();
+            int idUsuarioFilter = It.IsAny<int>();
+            string nomeFilter = _faker.Random.String2(100);
+            string descricaofilter = _faker.Random.String2(100);
+
+            var ativoList = new List<Ativo>()
+    {
+        autoFixture.Build<Ativo>().With(x => x.Transacoes, default(ICollection<Transacao>?)).With(x => x.Id, 1).Create()
+    };
+
+            _mockAtivoRepository.Setup(repo => repo.GetAllAsync(idFilter, idUsuarioFilter, nomeFilter, descricaofilter)).Returns(ativoList);
+
+            // Act
+            var result = _ativoService.GetAllAtivosAsync(idFilter, idUsuarioFilter, nomeFilter, descricaofilter);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [TestMethod]
+        public void AtivoListByFilter_RepositoryThrowsException_ShouldReturnThrowsExceptionWithMessage()
+        {
+            // Arrange
+            _mockAtivoRepository.Setup(repo => repo.GetAllAsync(It.IsAny<int?>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>()))
+                               .Throws(new Exception("Repository exception"));
+
+            // Act & Assert
+            try
+            {
+                _ativoService.GetAllAtivosAsync(It.IsAny<int?>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>());
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Erro na camada de serviço ao consultar Ativo por id. Mensagem de Erro: Repository exception", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void InsertAtivo_RepositoryThrowsException_ShouldReturnThrowsExceptionWithMessage()
+        {
+            // Arrange
+            var ativoDtoMock = new MockAtivoDTO();
+            var ativoDto = new CreateAtivoDTO { Nome = ativoDtoMock.Nome, Codigo = ativoDtoMock.Codigo };
+
+            _mockAtivoRepository.Setup(repo => repo.Insert(It.IsAny<Ativo>())).Throws(new Exception("Repository exception"));
+
+            // Act & Assert
+            var exception = Assert.ThrowsException<Exception>(() => _ativoService.AddAtivoAsync(ativoDto));
+            Assert.AreEqual("Erro na camada de serviço ao inserir Ativo. Mensagem de Erro: O Codigo deve ter no máximo 50 caracteres!", exception.Message);
+        }
+
+        [TestMethod]
+        public void UpdateAtivo_RepositoryThrowsException_ShouldReturnThrowsExceptionWithMessage()
+        {
+            // Arrange
+            var ativoDtoMock = new MockAtivoDTO();
+            var ativoDto = new UpdateAtivoDTO { Id = ativoDtoMock.Id, Nome = ativoDtoMock.Nome, Codigo = ativoDtoMock.Codigo };
+
+            _mockAtivoRepository.Setup(repo => repo.Update(It.IsAny<Ativo>())).Throws(new Exception("Repository exception"));
+
+            // Act & Assert
+            var exception = Assert.ThrowsException<Exception>(() => _ativoService.UpdateAtivoAsync(ativoDto));
+            Assert.AreEqual("Erro na camada de serviço ao atualizar Ativo. Mensagem de Erro: O Codigo deve ter no máximo 50 caracteres!", exception.Message);
         }
     }
 }
