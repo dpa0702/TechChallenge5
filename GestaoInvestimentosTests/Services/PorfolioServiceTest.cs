e7519d5584ddc4c4967a4f7c7b7b70835e0e8097
demetrios arghirachis <dpa0702@gmail.com>
2024-06-18 11:13:07 -0300

    Aumentado cobertura de testes.

--- GestaoInvestimentosTests/Services/PorfolioServiceTest.cs
+++ GestaoInvestimentosTests/Services/PorfolioServiceTest.cs
@@ -5,6 +5,8 @@ using GestaoInvestimentosCore.Entities;
 using GestaoInvestimentosCore.Interfaces.Repository;
 using GestaoInvestimentosCore.Services;
 using Moq;
+using GestaoInvestimentosCore.DTO.Portfolio;
+using GestaoInvestimentosTests.Entities.Mock;
 
 namespace GestaoInvestimentosTests.Services
 {
@@ -53,6 +55,78 @@ namespace GestaoInvestimentosTests.Services
             {
                 Assert.AreEqual("Erro na camada de serviço ao consultar Portfolio por id. Mensagem de Erro: Repository exception", ex.Message);
             }
+        }
+
+        [TestMethod]
+        public void PortfolioListByFilter_ShouldReturnSuccess()
+        {
+            // Arrange
+
+            var autoFixture = new Fixture().Customize(new AutoMoqCustomization());
+            int idFilter = It.IsAny<int>();
+            int idUsuarioFilter = It.IsAny<int>();
+            string nomeFilter = _faker.Random.String2(100);
+            string descricaofilter = _faker.Random.String2(100);
+
+            var portfolioList = new List<Portfolio>()
+            {
+                autoFixture.Build<Portfolio>().With(x => x.Transacoes, default(ICollection<Transacao>?)).With(x => x.Id, 1).Create()
+            };
+
+            _mockPortfolioRepository.Setup(repo => repo.GetAllAsync(idFilter, idUsuarioFilter, nomeFilter, descricaofilter)).Returns(portfolioList);
+
+            // Act
+            var result = _portfolioService.GetAllPortfoliosAsync(idFilter, idUsuarioFilter, nomeFilter, descricaofilter);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [TestMethod]
+        public void PortfolioListByFilter_RepositoryThrowsException_ShouldReturnThrowsExceptionWithMessage()
+        {
+            // Arrange
+            _mockPortfolioRepository.Setup(repo => repo.GetAllAsync(It.IsAny<int?>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>()))
+                               .Throws(new Exception("Repository exception"));
+
+            // Act & Assert
+            try
+            {
+                _portfolioService.GetAllPortfoliosAsync(It.IsAny<int?>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>());
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Erro na camada de serviço ao consultar Portfolio por id. Mensagem de Erro: Repository exception", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void InsertPortfolio_RepositoryThrowsException_ShouldReturnThrowsExceptionWithMessage()
+        {
+            // Arrange
+            var portfolioDtoMock = new MockPortfolioDTO();
+            var portfolioDto = new CreatePortfolioDTO { UsuarioId = portfolioDtoMock.UsuarioId, Nome = portfolioDtoMock.Nome, Descricao = portfolioDtoMock.Descricao };
+
+            _mockPortfolioRepository.Setup(repo => repo.Insert(It.IsAny<Portfolio>())).Throws(new Exception("Repository exception"));
+
+            // Act & Assert
+            var exception = Assert.ThrowsException<Exception>(() => _portfolioService.AddPortfolioAsync(portfolioDto));
+            Assert.AreEqual("Erro na camada de serviço ao inserir Portfolio. Mensagem de Erro: Repository exception", exception.Message);
+        }
+
+        [TestMethod]
+        public void UpdatePortfolio_RepositoryThrowsException_ShouldReturnThrowsExceptionWithMessage()
+        {
+            // Arrange
+            var portfolioDtoMock = new MockPortfolioDTO();
+            var portfolioDto = new UpdatePortfolioDTO { Id = portfolioDtoMock.Id, UsuarioId = portfolioDtoMock.UsuarioId, Nome = portfolioDtoMock.Nome, Descricao = portfolioDtoMock.Descricao };
+
+            _mockPortfolioRepository.Setup(repo => repo.Update(It.IsAny<Portfolio>())).Throws(new Exception("Repository exception"));
+
+            // Act & Assert
+            var exception = Assert.ThrowsException<Exception>(() => _portfolioService.UpdatePortfolioAsync(portfolioDto));
+            Assert.AreEqual("Erro na camada de serviço ao atualizar Portfolio. Mensagem de Erro: Repository exception", exception.Message);
         }
     }
 }
--- GestaoInvestimentosTests/Services/UsuarioServiceTest.cs
+++ GestaoInvestimentosTests/Services/UsuarioServiceTest.cs
@@ -6,6 +6,8 @@ using GestaoInvestimentosCore.Interfaces.Services;
 using GestaoInvestimentosCore.Services;
 using Moq;
 using GestaoInvestimentosCore.Entities;
+using GestaoInvestimentosCore.DTO.Usuario;
+using GestaoInvestimentosTests.Entities.Mock;
 
 namespace GestaoInvestimentosTests.Services
 {
@@ -56,6 +58,78 @@ namespace GestaoInvestimentosTests.Services
             {
                 Assert.AreEqual("Erro na camada de serviço ao consultar Usuarioo por id. Mensagem de Erro: Repository exception", ex.Message);
             }
+        }
+
+        [TestMethod]
+        public void UsuarioListByFilter_ShouldReturnSuccess()
+        {
+            // Arrange
+
+            var autoFixture = new Fixture().Customize(new AutoMoqCustomization());
+            int idFilter = It.IsAny<int>();
+            string nomeFilter = _faker.Random.String2(100);
+            string emailfilter = _faker.Random.String2(100);
+            string senhafilter = _faker.Random.String2(100);
+
+            var usuarioList = new List<Usuario>()
+            {
+                autoFixture.Build<Usuario>().With(x => x.Id, idFilter).Create()
+            };
+
+            _mockUsuarioRepository.Setup(repo => repo.GetAllAsync(idFilter, nomeFilter, emailfilter, senhafilter)).Returns(usuarioList);
+
+            // Act
+            var result = _usuarioService.GetAllUsuariosAsync(idFilter, nomeFilter, emailfilter, senhafilter);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [TestMethod]
+        public void UsuarioListByFilter_RepositoryThrowsException_ShouldReturnThrowsExceptionWithMessage()
+        {
+            // Arrange
+            _mockUsuarioRepository.Setup(repo => repo.GetAllAsync(It.IsAny<int?>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                               .Throws(new Exception("Repository exception"));
+
+            // Act & Assert
+            try
+            {
+                _usuarioService.GetAllUsuariosAsync(It.IsAny<int?>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>());
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Erro na camada de serviço ao consultar Usuario por id. Mensagem de Erro: Repository exception", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void InsertUsuario_RepositoryThrowsException_ShouldReturnThrowsExceptionWithMessage()
+        {
+            // Arrange
+            var usuarioDtoMock = new MockUsuarioDTO();
+            var usuarioDto = new CreateUsuarioDTO { Nome = usuarioDtoMock.Nome, Email = usuarioDtoMock.Email, Senha = usuarioDtoMock.Senha };
+
+            _mockUsuarioRepository.Setup(repo => repo.Insert(It.IsAny<Usuario>())).Throws(new Exception("Repository exception"));
+
+            // Act & Assert
+            var exception = Assert.ThrowsException<Exception>(() => _usuarioService.AddUsuarioAsync(usuarioDto));
+            Assert.AreEqual("Erro na camada de serviço ao inserir Usuario. Mensagem de Erro: Repository exception", exception.Message);
+        }
+
+        [TestMethod]
+        public void UpdateUsuario_RepositoryThrowsException_ShouldReturnThrowsExceptionWithMessage()
+        {
+            // Arrange
+            var usuarioDtoMock = new MockUsuarioDTO();
+            var usuarioDto = new UpdateUsuarioDTO { Id = usuarioDtoMock.Id, Nome = usuarioDtoMock.Nome, Email = usuarioDtoMock.Email, Senha = usuarioDtoMock.Senha };
+
+            _mockUsuarioRepository.Setup(repo => repo.Update(It.IsAny<Usuario>())).Throws(new Exception("Repository exception"));
+
+            // Act & Assert
+            var exception = Assert.ThrowsException<Exception>(() => _usuarioService.UpdateUsuarioAsync(usuarioDto));
+            Assert.AreEqual("Erro na camada de serviço ao atualizar Usuario. Mensagem de Erro: Repository exception", exception.Message);
         }
     }
 }
